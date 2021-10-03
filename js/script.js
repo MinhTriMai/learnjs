145e8d03648e26632a617db9a959b16fa6a98ed9
Tri Mai Minh <mingti@Tris-MacBook-Pro.local>
2021-10-03 14:53:35 +0700

    add a list to script file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'Noodle Rice';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: '0.7', types: ['grass', 'poison']},
+    {name: 'Ivysaur', height: '1', types: ['grass', 'poison']},
+    {name: 'Squirtle', height: '0.5', types: ['water']}
+];
