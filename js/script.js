7c02d2f4dc83317b67bced6dd949ff2dc98d1d88
Tri Mai Minh <mingti@Tris-MacBook-Pro.local>
2021-10-04 23:00:40 +0700

    add new code for listing pokemon in array

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ let pokemonList = [
     {name: 'Ivysaur', height: '1', types: ['grass', 'poison']},
     {name: 'Squirtle', height: '0.5', types: ['water']}
 ];
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write(pokemonList[i].name + pokemonList[i].height);
+    
+// }
+
+//Listing array of pokemon
+for (let i=0; i<pokemonList.length; i++) {
+
+    let pokemonName = pokemonList[i].name;
+    let pokemonHeight = pokemonList[i].height;
+
+    let note;
+    if (pokemonHeight >= 1) {
+        note = 'Wow, that’s big!'
+    }
+    
+    let result = `<br>${pokemonName} (Height: ${pokemonHeight}) ${note || ''}<br>` ;
+    
+    document.write(result);
+}
